28a0d0d61f4af095d5aeaa0c1e0bc02f9d3011af
mtopalid <meropi.topalidou@inria.fr>
2016-01-11 15:34:22 +0100

    Updated the task choice in order to give all the targets to all initial positions. The number of the trials should be multiple of 81.

--- cython/task_position.py
+++ cython/task_position.py
@@ -48,77 +48,104 @@ class Task(object):
                                    ("target", int, 1)])
         self.records = np.zeros(n, [("best", float, 1),
                                     ("RTmove", float, 1),
+                                    ("move", int, 1),
                                     ("moves", int, 1),
                                     ("reward", float, 1),
                                     ("target_pos", float, 2),
-                                    ("final_pos", float, 2)])
+                                    ("final_pos", float, 2),
+                                    ("SMAValues", float, 405),
+                                    ("PPCValues", int, 405),
+                                    ("Wppc_sma", float, 405),
+                                    ("Wsma_str", float, 405),
+                                    ("Wppc_str", float, 405)])#("Wtrg_ism", float, 81)
 
-    def process(self, trial, action, RT=0, debug=False):
+    def process(self, RT=0, debug=False):
 
         m = self.records[self.index]["final_pos"]
         t = self.records[self.index]["target_pos"]
-        moves = self.records[self.index]["moves"]
 
-        if debug:
-            print("Trial %d" % (self.index + 1))
-            # print("  Action                : %d " % action)
         if (m == t).all():
-            reward = 1
             best = True
-            if debug:
-                print("  Position has been reached")
+            # if debug:
+            #     print("  Position has been reached")
         else:
-            reward = 0.
             best = False
-            if debug:
-                print("  Position has NOT been reached")
+            # if debug:
+            #     print("  Position has NOT been reached")
 
         # Record action, best action (was it the best action), reward and RT
         self.records[self.index]["best"] = best
         self.records[self.index]["RTmove"] = RT
-        self.records[self.index]["reward"] = reward
 
-        if debug:
-            P = self.records[:self.index + 1]["best"]
-            print(("  Mean performance		: %.1f %%" % np.array(P * 100).mean()))
-            R = self.records[:self.index + 1]["reward"]
-            print(("  Mean reward			: %.3f" % np.array(R).mean()))
-            rt = self.records[self.index]["RTmove"] - self.records[self.index]["RTmot"]
-            print(("  Response time	move    : %.3f ms" % np.array(rt)))
-            n_moves = self.records[self.index]["moves"]
-            print(("  Number of moves       : %d" % n_moves))
-            rt = self.records[:self.index + 1]["RTmove"]
-            print(("  Mean Response time	move: %.3f ms" % np.array(rt).mean()))
+    def debugging(self):
+        print("Trial %d" % (self.index + 1))
 
-        return reward, best
+        if self.records[self.index]["best"]:
+            print("  Position has been reached")
+        else:
+            print("  Position has NOT been reached")
 
-    def setup(self, n=9):
+        P = self.records[:self.index + 1]["best"]
+        print(("  Mean performance		: %.1f %%" % np.array(P * 100).mean()))
+        r = self.records[self.index]["reward"]
+        print(("  Reward    			: %f" % r))
+        R = self.records[:self.index + 1]["reward"]
+        print(("  Mean reward			: %.3f" % np.array(R).mean()))
+        rt = self.records[self.index]["RTmove"]
+        print(("  Response time	move    : %.3f ms" % np.array(rt)))
+        initpos = self.trials[self.index]["initial_pos_arm"]
+        print(("  Initial position      : %d" % initpos))
+        trg = self.trials[self.index]["target"]
+        print(("  Target                : %d" % trg))
+        move = self.records[self.index]["move"]
+        print(("  Chosen Move           : %d" % move))
+        rt = self.records[:self.index + 1]["RTmove"]
+        print(("  Mean Response time move: %.3f ms" % np.array(rt).mean()))
+        moves = self.records[self.index]["moves"]
+        print(("  Moves                 : %d" % moves))
+        print("\n\n")
+
+    def setup(self, n=81):
 
         # Make sure count is a multiple of 6
-        n = (n // 9) * 9
+        n = (n // 81) * 81
         self.build(n)
 
         # n//9 x all combinations of initial positions of the arm
-        ipa = np.repeat(np.arange(9), n // 9)
-        np.random.shuffle(ipa)
+        # ipa = np.repeat(np.arange(9), n // 9)
+        # np.random.shuffle(ipa)
 
-        # n//9 x all combinations of target positions
-        tar = np.repeat(np.arange(9), n // 9)
-        np.random.shuffle(tar)
+        # Z = np.array([1, 3, 5, 7])
+        #
+        # # n//4 x all combinations of positions
+        # temp = np.repeat(np.arange(4), n // 4)
+        # np.random.shuffle(temp)
+        # tar = Z[temp]
+        # np.random.shuffle(tar[:])
+        # tar = np.repeat(np.arange(9), n // 9)
+        # np.random.shuffle(tar)
+
+        cues = np.zeros((n, 2))
+        temp = np.repeat(np.arange(9), n // 9)
+        cues[:, 0] = 4#temp
+        temp = np.arange(9)
+        nn = n // 9
+        for i in range(nn):
+            cues[i * 9:(i + 1) * 9, 1] = temp
+        np.random.shuffle(cues)
 
         for i in range(n):
-            trial = self.trials[i]
 
-            trial["initial_pos_arm"] = ipa[i]
-            trial["target"] = tar[i]
+            trial = self.trials[i]
+            trial["initial_pos_arm"] = cues[i, 0]
+            trial["target"] = cues[i, 1]
 
 
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
     import random
 
-
-    task = Task(n=9)
+    task = Task(n=81)
 
     for trial in task:
         # Only the associative group can provide (m1,c1), (m2,c2)
